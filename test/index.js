ae88a1c67d3f8769c8e19e21d50ffbbde2d92e98
Diana Thayer <garbados@gmail.com>
2017-12-10 10:59:02 -0800

    feat: add support for modulo

--- test/index.js
+++ test/index.js
@@ -2,10 +2,25 @@ const tap = require('tap')
 const NumbersStation = require('../lib')
 
 tap.test('numbers-station', (test) => {
-  const ns = new NumbersStation()
+  const ns = new NumbersStation(1, 1000, 6)
+  // ticker generates random numbers as soon as it starts
   test.equal(ns.number, undefined)
   ns.start()
   test.notEqual(ns.number, undefined)
+  // ensure numbers obey the modulo
+  var maxNumber = [
+    null,
+    null,
+    null,
+    null,
+    null
+  ].map(() => {
+    return ns.getNumber()
+  }).reduce((a, b) => {
+    return Math.max(a, b)
+  }, 0)
+  test.ok(maxNumber < ns.mod)
+  // wait for ticker
   const testNumber1 = ns.number
   setTimeout(() => {
     const testNumber2 = ns.number
