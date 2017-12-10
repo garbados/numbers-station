e08dfc9b5355c16eb2a3a65643f651b8eb58b7a4
Diana Thayer <garbados@gmail.com>
2017-12-10 13:16:58 -0800

    feat: use proper csprng

--- test/index.js
+++ test/index.js
@@ -2,11 +2,16 @@ const tap = require('tap')
 const NumbersStation = require('../lib')
 
 tap.test('numbers-station', (test) => {
-  const ns = new NumbersStation(100, 1000, 100)
+  const ns = new NumbersStation(100, 1000)
+
   // ticker generates random numbers as soon as it starts
   test.equal(ns.number, undefined)
   ns.start()
   test.notEqual(ns.number, undefined)
+
+  // ensure promises are being resolved
+  test.equal(typeof ns.number, 'number')
+
   // ensure numbers obey the modulo
   var maxNumber = [
     null,
@@ -19,7 +24,8 @@ tap.test('numbers-station', (test) => {
   }).reduce((a, b) => {
     return Math.max(a, b)
   }, 0)
-  test.ok(maxNumber < ns.mod)
+  test.ok(maxNumber < ns.maximum)
+
   // wait for ticker
   const testNumber1 = ns.number
   setTimeout(() => {
